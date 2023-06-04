0f468bc2a929ba4e032df8b93087e73dc5967200
c-desouza <118620391+c-desouza@users.noreply.github.com>
2023-06-04 23:14:18 +0530

    Randomly select four animals from array
    If an animal is chosen once it cannot be chosen again.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* PSEUDO CODE */
+
+/* 
+1. store the nmaes and data-value of each animal in an object and then store all the objects in an array
+2. randomly generate two instances of each animal
+   2.1 Use the random function and generate four numbers from 0-15
+   2.2 the animal associated with one of the four numbers will be  one among the four animals, the rest will be chosen in same way
+3. populate a different array with each 2 copies of the above selected numbers
+4. generate a random number between one and 8 (inclusive), store the previous number(image) at that location in grid
+5. If a number already chosen you can't choose it again.  
+
+*/
 const animals = [
   "bear",
   "camel",
@@ -16,3 +28,13 @@ const animals = [
   "ram",
   "squirrel",
 ];
+
+const chosenAnimals = [];
+
+for (let i = 1; i <= 4; i++) {
+  let randomIndex = Math.floor(Math.random() * 16);
+  while (chosenAnimals.includes(randomIndex)) {
+    randomIndex = Math.floor(Math.random() * 16);
+  }
+  chosenAnimals.push(randomIndex);
+}
