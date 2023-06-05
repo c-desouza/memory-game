e1b5b0b4a07ca0185a8b1a6c55675fea14be3970
c-desouza <118620391+c-desouza@users.noreply.github.com>
2023-06-05 22:59:05 +0530

    Add animal image behind each card

--- script.js
+++ script.js
@@ -68,6 +68,74 @@ for (let i = 0; i < 8; i++) {
   const animal = document.createElement("div");
   animal.classList.add("animal");
   animalGridContainer.appendChild(animal);
+  switch (grid[i]) {
+    case 0:
+      animal.style.backgroundImage = "url(images/bear.png)";
+      break;
+
+    case 1:
+      animal.style.backgroundImage = "url(images/camel.png)";
+      break;
+
+    case 2:
+      animal.style.backgroundImage = "url(images/cat.png)";
+      break;
+
+    case 3:
+      animal.style.backgroundImage = "url(images/dog.png)";
+      break;
+
+    case 4:
+      animal.style.backgroundImage = "url(images/elephant.png)";
+      break;
+
+    case 5:
+      animal.style.backgroundImage = "url(images/fox.png)";
+      break;
+
+    case 6:
+      animal.style.backgroundImage = "url(images/giraffe.png)";
+      break;
+
+    case 7:
+      animal.style.backgroundImage = "url(images/horse.png)";
+      break;
+
+    case 8:
+      animal.style.backgroundImage = "url(images/lion.png)";
+      break;
+
+    case 9:
+      animal.style.backgroundImage = "url(images/owl.png)";
+      break;
+
+    case 10:
+      animal.style.backgroundImage = "url(images/panda.png)";
+      break;
+
+    case 11:
+      animal.style.backgroundImage = "url(images/penguin.png)";
+      break;
+
+    case 12:
+      animal.style.backgroundImage = "url(images/pig.png)";
+      break;
+
+    case 13:
+      animal.style.backgroundImage = "url(images/rabbit.png)";
+      break;
+
+    case 14:
+      animal.style.backgroundImage = "url(images/ram.png)";
+      break;
+
+    case 15:
+      animal.style.backgroundImage = "url(images/squirrel.png)";
+      break;
+
+    default:
+      break;
+  }
 }
 
 body.appendChild(animalGridContainer);
